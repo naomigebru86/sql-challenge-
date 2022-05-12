b85093c4ef4f6bb7f09b0ea80bd28fa9768c45b0
naomigebru86 <95658342+naomigebru86@users.noreply.github.com>
2022-05-12 16:12:44 -0400

    Update Homework.sql

--- Homework.sql
+++ Homework.sql
@@ -17,3 +17,43 @@ INNER JOIN employees ON employees.emp_no=dept_emp.emp_no
 INNER JOIN departments ON departments.dept_no=manager.dept_no
 -- #4 List the department of each employee with the following information: 
 -- employee number, last name, first name, and department name.
+select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+from employees
+join dept_emp on
+employees.emp_no=dept_emp.emp_no
+join departments on
+departments.dept_no=dept_emp.dept_no;
+--#5 List first name, last name, and sex for employees 
+--whose first name is "Hercules" and last names begin with "B."
+select first_name, last_name, sex
+from employees
+where first_name like 'Hercules' and last_name like 'B%';
+--#6 List all employees in the Sales department, 
+--including their employee number, last name, first name, and department name.
+select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+from employees
+join dept_emp on
+employees.emp_no=dept_emp.emp_no
+join departments on
+departments.dept_no=dept_emp.dept_no
+where dept_name like 'Sales';
+
+
+--#7 List all employees in the Sales and Development departments, 
+--including their employee number, last name, first name, and department name.
+select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+from employees
+join dept_emp on
+employees.emp_no=dept_emp.emp_no
+join departments on
+departments.dept_no=dept_emp.dept_no
+where dept_name like 'Sales' or dept_name like 'Development';
+
+
+--#8 In descending order, list the frequency count of employee last names,
+--i.e., how many employees share each last name.
+select last_name, count(last_name) as "Last name frequency"
+from employees
+group by last_name
+order by "Last name frequency" desc;
+
